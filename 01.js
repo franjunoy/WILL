ba727a63d40e8cdab58576c8cfa0f3fee35383dc
franjunoy <franjunoy@hotmail.com>
2023-04-07 03:25:15 -0300

    checkpoint commit

--- 01.js
+++ 01.js
@@ -11,7 +11,13 @@ function soloNumeros(array) {
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
-
+  nuevoArray = [];
+  for(var i = 0; i < array.length; i++){
+    if(typeof array[i] === "number"){
+      nuevoArray.push(array[i]);
+    }
+  }
+  return nuevoArray;
 }
 
 // No modifiques nada debajo de esta linea //
